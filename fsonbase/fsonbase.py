9f755bd4ba7a310c2e2c16f9775f7615390e2f10
Fadix47 <101798299+Fadix47@users.noreply.github.com>
2022-09-29 18:58:02 +0500

    Update fsonbase.py

--- fsonbase/fsonbase.py
+++ fsonbase/fsonbase.py
@@ -8,31 +8,32 @@ from json import dump
 
 class fsonbase:
     def __init__(self, filepath):
+        filepath = filepath.replace('\\', '/')
         if isdir(filepath):
             self.filepath = filepath
-            self.clusters = tuple([fcluster(fr"{self.filepath}\{x}").name for x in listdir(self.filepath) if Path(x).suffix == '.json'])
+            self.clusters = tuple([fcluster(fr"{self.filepath}/{x}").name for x in listdir(self.filepath) if Path(x).suffix == '.json'])
         else:
             raise TypeError("Invalid filepath. Should be directory")
 
     def connect(self, cluster: str) -> fcluster:
-        if isfile(fr"{self.filepath}\{cluster}.json"):
-            return fcluster(fr"{self.filepath}\{cluster}.json")
+        if isfile(fr"{self.filepath}/{cluster}.json"):
+            return fcluster(fr"{self.filepath}/{cluster}.json")
         else:
             raise FileNotFoundError('Cluster not found')
 
     def create(self, cluster: str) -> fcluster:
-        if not exists(fr"{self.filepath}\{cluster}.json"):
-            with open(fr"{self.filepath}\{cluster}.json", 'w') as file: dump({}, file, ensure_ascii=False, indent=4)
+        if not exists(fr"{self.filepath}/{cluster}.json"):
+            with open(fr"{self.filepath}/{cluster}.json", 'w') as file: dump({}, file, ensure_ascii=False, indent=4)
 
-            return fcluster(fr"{self.filepath}\{cluster}.json")
+            return fcluster(fr"{self.filepath}/{cluster}.json")
         else:
             raise FileExistsError("Cluster already exists!")
 
     def delete(self, cluster: Union[fcluster, str]) -> None:
-        name = fcluster(fr"{self.filepath}\{cluster}.json").name if isinstance(cluster, str) else cluster.name
+        name = fcluster(fr"{self.filepath}/{cluster}.json").name if isinstance(cluster, str) else cluster.name
         print(name)
 
-        if exists(fr"{self.filepath}\{name}.json"):
-            remove(fr"{self.filepath}\{name}.json")
+        if exists(fr"{self.filepath}/{name}.json"):
+            remove(fr"{self.filepath}/{name}.json")
         else:
             raise FileNotFoundError("Cluster does not exist!")
